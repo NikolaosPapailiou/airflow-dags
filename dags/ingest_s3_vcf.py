8d7ba03b3ad94887d9fe3fda186ff2edfa43cdfc
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2022-11-14 18:04:58 +0200

    filter bcf files

--- dags/ingest_s3_vcf.py
+++ dags/ingest_s3_vcf.py
@@ -62,7 +62,7 @@ def ingest_s3_vcf():
     @task
     def partition_files(s3_files, chunk_size):
         p = re.compile('.*\.bcf')
-        bcf_files = [ f for f in s3_files if p.match(s) ]
+        bcf_files = [ f for f in s3_files if p.match(f) ]
         return list(split(bcf_files, chunk_size))
 
     partitions = partition_files(XComArg(s3_files), 10)
