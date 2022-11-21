95605aec292c8bd666adb2787c80edcddf073b89
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2022-11-21 16:04:46 +0200

    Test TileDB ingestion

--- dags/s3_vcf_to_tiledb.py
+++ dags/s3_vcf_to_tiledb.py
@@ -46,8 +46,8 @@ dag_params = {
     's3_prefix': 'gvcfs/1000',
     's3_conn_id': 'aws',
     'tiledb_array_uri': 's3://tiledb-nikos/arrays/test-vcf',
-    'tiledb_worker_memory': '2048',
-    'vcf_files_per_worker': '2'
+    'tiledb_worker_memory': Param(2048, type="integer", minimum=1024),
+    'vcf_files_per_worker': Param(2, type="integer", minimum=1)
 }
 # [END default_args]
 
@@ -110,7 +110,7 @@ def s3_vcf_to_tiledb():
         )
 
     create_array(array_uri="{{ params.tiledb_array_uri }}")
-    partitions = partition_files(XComArg(s3_files), int("{{ params.vcf_files_per_worker }}"))
+    partitions = partition_files(XComArg(s3_files), "{{ params.vcf_files_per_worker }}")
     ingest_vcf_to_tiledb.partial(array_uri="{{ params.tiledb_array_uri }}").expand(files=partitions)
 
 # [START dag_invocation]
